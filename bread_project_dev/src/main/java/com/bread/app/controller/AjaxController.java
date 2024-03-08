6d722129f23730bc6387057378f395bd5f7bb1b9
yhkwak <154938948+yhkwak@users.noreply.github.com>
2024-03-08 19:27:07 +0900

    Update AjaxController.java

--- bread_project_dev/src/main/java/com/bread/app/controller/AjaxController.java
+++ bread_project_dev/src/main/java/com/bread/app/controller/AjaxController.java
@@ -7,9 +7,12 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +28,11 @@ import com.bread.app.vo.CartVO;
 import com.bread.app.vo.LikesVO;
 import com.bread.app.vo.MemberVO;
 import com.bread.service.member.MemberService;
+import com.bread.service.sms.SmsService;
 import com.bread.service.order.OrderService;
 
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
