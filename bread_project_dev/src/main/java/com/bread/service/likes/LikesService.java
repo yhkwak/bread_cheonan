b55e45365faf3c7a5023019f1513df1977b8fc8f
soohee <soohee@220.83.219.165>
2024-03-08 20:10:34 +0900

    매장 찜하기 추가수정

--- bread_project_dev/src/main/java/com/bread/service/likes/LikesService.java
+++ bread_project_dev/src/main/java/com/bread/service/likes/LikesService.java
@@ -7,13 +7,11 @@ public interface LikesService {
 	//매장 찜 여부 확인
 	default int checkBL(LikesVO lvo) {return 0;}
 	default int checkBL2(LikesVO lvo) {return 0;}
+	
 	//매장 찜하기
 	default int addBL(LikesVO lvo) {return 0;}
 	
 	//매장 찜 해제
-	default void delBL(LikesVO lvo) {return ;}
+	default int delBL(LikesVO lvo) {return 0;}
 	
-	// 매장 찜 상태 업데이트
-	default int updateBL(LikesVO lvo) {return 0;}
-
 }
