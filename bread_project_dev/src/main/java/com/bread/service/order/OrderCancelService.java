7583e7160b227d14dbc46fbd5e9faed17af2ce5a
joo <white3652@naver.com>
2024-03-11 09:45:01 +0900

    주문취소 테스트용 alert창 뜨지않도록 수정완료

--- bread_project_dev/src/main/java/com/bread/service/order/OrderCancelService.java
+++ bread_project_dev/src/main/java/com/bread/service/order/OrderCancelService.java
@@ -8,7 +8,6 @@ import com.bread.app.dao.OrderDAO;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service("oCancel")
 @AllArgsConstructor
 public class OrderCancelService implements OrderService {
@@ -23,9 +22,7 @@ public class OrderCancelService implements OrderService {
         
         try {
             result = dao.orderCancel(order_idx.trim());
-            System.out.println("서비스 result : "+result);
-            System.out.println("서비스 order_idx:" + order_idx);
-        	
+
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
--- bread_project_dev/src/main/webapp/resources/js/orderManagement.js
+++ bread_project_dev/src/main/webapp/resources/js/orderManagement.js
@@ -1,11 +1,10 @@
 function orderCancel() {
 	const order_idx = $("#button-write").attr("data-value");
-	alert(order_idx);
+
 
     const confirmAns = confirm("정말 주문 취소를 하시겠습니까?");
     
     if (confirmAns) {
-    console.log("자바스크립트 order_idx 값:", order_idx);
         $.ajax({
             type: "POST",
             url: "/myapp/mypage/orderCancelProcess.do",
